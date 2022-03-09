5e7da8b25cb63e17f82b3f21d9174da7f842f159
Marcelino Ioshio Kuzuhara <mik.rikari@hotmail.com>
2022-03-09 15:35:38 -0300

    Versão 3 - Adição de escolha de conversão real-dólar e real-euro

--- 02Moedas/script.js
+++ 02Moedas/script.js
@@ -3,9 +3,21 @@ function Converter() {
   var valor = pegaElemento.value;
   var valorEmReal = parseFloat(valor);
 
-  var valorEmDolar = valorEmReal / 5;
+  var pegaOpcao = document.querySelector('input[name="moeda"]:checked');
+  var moeda = pegaOpcao.value;
+  console.log(moeda);
+  var valorConvertido;
+  var textoResultado;
+  if (moeda == "dolar") {
+    valorConvertido = valorEmReal / 5;
+    textoResultado = "O valor em dólares é US$ " + valorConvertido.toFixed(2);
+  }
+  if (moeda == "euro") {
+    valorConvertido = valorEmReal / 6;
+    textoResultado =
+      "O valor em euros é " + valorConvertido.toFixed(2) + " euros";
+  }
 
-  pegaValorConvertido = document.getElementById("valorConvertido");
-  textoResultado = "O valor em dólar é US$ " + valorEmDolar.toFixed(2);
-  pegaValorConvertido.innerHTML = textoResultado;
+  var pegaResultado = document.getElementById("valorConvertido");
+  pegaResultado.innerHTML = textoResultado;
 }
