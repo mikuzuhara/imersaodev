bbd7d4508cfa2e7254492bcc38dd36543c3ee96a
Marcelino Ioshio Kuzuhara <mik.rikari@hotmail.com>
2022-03-13 22:13:58 -0300

    Versão 2 - Desafios do dia 5 AluraFlix concluídos

--- 05AluraFlix/script.js
+++ 05AluraFlix/script.js
@@ -1,20 +1,71 @@
-var imagemFilme;
 var campoFilmeFavorito = document.getElementById("filme");
+var campoNomeFilme = document.getElementById("nomeFilme");
+var filmeFavorito, nomeFilme;
+
 campoFilmeFavorito.focus();
+var listaFilmes = [];
+var vetorFilmes = document.getElementById("listaFilmes");
 
 function adicionarFilme() {
   function listarFilmes() {
-    imagemFilme = "<img src=" + filmeFavorito + ">";
-    var listaFilmes = document.getElementById("listaFilmes");
-    listaFilmes.innerHTML += imagemFilme;
+    function createDeleteButton(index) {
+      function deleteRegister() {
+        listaFilmes.splice(index, 1);
+        listarFilmes();
+      }
+      var button = document.createElement("button");
+      button.classList.add("deleteButton");
+      button.textContent = "x";
+      button.addEventListener("click", deleteRegister);
+
+      return button;
+    }
+    function createSpan(name) {
+      var span = document.createElement("span");
+      span.textContent = name;
+
+      return span;
+    }
+    vetorFilmes.innerHTML = "";
+    for (var i = 0; i < listaFilmes.length; i++) {
+      var image = document.createElement("img");
+      image.src = listaFilmes[i][0];
+
+      var div = document.createElement("div");
+      div.classList.add("registro");
+      var divGrade = document.createElement("div");
+      divGrade.classList.add("divName");
+
+      var span = createSpan(listaFilmes[i][1]);
+      var button = createDeleteButton(i);
+
+      divGrade.appendChild(button);
+      divGrade.appendChild(span);
+
+      div.appendChild(image);
+      div.appendChild(divGrade);
+      vetorFilmes.appendChild(div);
+    }
   }
 
+  var registro = [];
   campoFilmeFavorito.focus();
-  var filmeFavorito = campoFilmeFavorito.value;
+  filmeFavorito = campoFilmeFavorito.value;
+  nomeFilme = campoNomeFilme.value;
+
   if (filmeFavorito.startsWith("data:image/jpeg")) {
+    registro.push(filmeFavorito);
+    registro.push(nomeFilme);
+    listaFilmes.push(registro);
     listarFilmes();
   } else {
     alert("Isso não é uma imagem!");
   }
-  campoFilmeFavorito.value = "";
+  clearInput();
+}
+
+function clearInput() {
+  document.getElementById("filme").value = "";
+  document.getElementById("nomeFilme").value = "";
+  document.getElementById("filme").focus();
 }
